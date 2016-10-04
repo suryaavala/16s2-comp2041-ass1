4670cb4e4aa76f9758f4ef5ded8d984ced916da0
Surya Avala <suryaavala.dev@gmail.com>
2016-10-04 20:53:29 +1100

    working on cookie0.pl, added global indentation

--- plpy.pl
+++ plpy.pl
@@ -2,14 +2,33 @@
 
 # written by Surya Avinash Avala, z0596886
 # for COMP9041, 16s2, Assignment 1
+my $filename = 'temp.txt';
+open(my $fh, '+>', $filename)
+  or die "Could not open file '$filename' $!";
+while ($line = <>){
 
-while ($line = <>) {
-    if ($line =~ /^#!/ && $. == 1) {
+  if ($line =~ /<STDIN>/) {
+    print "import sys\n";
+    print $fh $line;
+  } elsif ($line =~ /^#!/ && $. == 1) {
 
-        # translate #! line
-        #print "1\n";
-        print "#!/usr/local/bin/python3.5 -u\n";
-    } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+      # translate #! line
+      #print "1\n";
+      print "#!/usr/local/bin/python3.5 -u\n";
+  } else {
+    print $fh $line;
+  }
+}
+
+close $fh;
+open($fh, '<', $filename)
+  or die "Could not open file '$filename' $!";
+
+while ($line = <$fh>) {
+   #printing indentation
+   $line =~ /^(\s*).*$/g;
+   $indent = substr $1,0,-1;
+   if ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 
         # Blank & comment lines can be passed unchanged
         #print "2\n";
@@ -18,8 +37,8 @@ while ($line = <>) {
         # Python's print adds a new-line character by default
         # so we need to delete it from the Perl print statement
         $new_line = $1;
-        $line =~ /^(.*)print.*$/g;
-        $indent = substr $1,0,-1;
+        #$line =~ /^(.*)print.*$/g;
+        #$indent = substr $1,0,-1;
         if ($new_line =~ /\$/) {
           #if printing a variable then strip $ and ""
           $new_line =~ tr/\$"//d;
@@ -35,8 +54,8 @@ while ($line = <>) {
     } elsif ($line =~ /^\s*print\s*(.*)"\\n"[\s;]*$/) {
       #printing variables
       $new_line = $1;
-      $line =~ /^(.*)print.*$/g;
-      $indent = substr $1,0,-1;
+      #$line =~ /^(.*)print.*$/g;
+      #$indent = substr $1,0,-1;
       $new_line =~ tr/\$,//d;
       $new_line =~ s/^\s+|\s+$//g;
       print "$indent";
@@ -44,25 +63,35 @@ while ($line = <>) {
     } elsif ($line =~ /^\s*if\s*\((.*)\)\s*{$/) {
       #dealing with if conditions
       $new_line = $1;
+      #$line =~ /^(.*)if.*$/g;
+      #$indent = substr $1,0,-1;
       $new_line =~ tr/\$//d;
+      print "$indent";
       print "if $new_line:\n";
     } elsif ($line =~ /^\s*while\s*\((.*)\)\s*{$/){
       #dealing with while loops
       $new_line = $1;
+      #$line =~ /^(.*)while.*$/g;
+      #$indent = substr $1,0,-1;
       $new_line =~ tr/\$//d;
+      print "$indent";
       print "while $new_line:\n";
     } elsif ($line =~ /\$.*[\s;]$/) {
       #python variable assignment and variable manipulation
       $new_line = substr $line,1,-2;
       $new_line =~ tr/\$//d;
       print "$new_line\n";
-    } elsif ($line =~ /^}/) {
+    } elsif ($line =~ /}/) {
       #ignore closing braces and move on
       next;
     }
     else {
         # Lines we can't translate are turned into comments
-
-        print "#$line\n";
+        #$line =~ /^(\s*)/;
+        #$indent = substr $1,0,-1;
+        print "$indent";
+        print "$line\n";
     }
 }
+
+close $fh;
