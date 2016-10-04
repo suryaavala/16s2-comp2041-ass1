57e9490f87440c95cc3c26addfddf0ae0bdd6af4
Surya Avala <suryaavala.dev@gmail.com>
2016-10-04 19:32:30 +1100

    subset 0,1 working

--- plpy.pl
+++ plpy.pl
@@ -7,19 +7,41 @@ while ($line = <>) {
     if ($line =~ /^#!/ && $. == 1) {
 
         # translate #! line
-
-        print "#!/usr/local/bin/python3.5 -u";
+        #print "1\n";
+        print "#!/usr/local/bin/python3.5 -u\n";
     } elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
 
         # Blank & comment lines can be passed unchanged
-
+        #print "2\n";
         print $line;
     } elsif ($line =~ /^\s*print\s*"(.*)\\n"[\s;]*$/) {
         # Python's print adds a new-line character by default
         # so we need to delete it from the Perl print statement
+        $new_line = $1;
+        if ($1 =~ /\$/) {
+          #if printing a variable then strip $ and ""
+          $new_line =~ tr/\$"//d;
+          #print "3\n";
+          print "print($new_line)\n";
+        }
+        else {
+        #print "4\n";
+        print "print(\"$new_line\")\n";
+        }
+    } elsif ($line =~ /^\s*print\s*(.*)"\\n"[\s;]*$/) {
+      #printing variables
+      $new_line = $1;
+      $new_line =~ tr/\$,//d;
+      $new_line =~ s/^\s+|\s+$//g;
+      print "print($new_line)\n";
+    }  elsif ($line =~ /^\$.*[\s;]$/) {
+      #python variable assignment
+      $new_line = substr $line,1,-2;
+      $new_line =~ tr/\$//d;
+      print "$new_line\n";
+    }
 
-        print "print(\"$1\")\n";
-    } else {
+    else {
         # Lines we can't translate are turned into comments
 
         print "#$line\n";
